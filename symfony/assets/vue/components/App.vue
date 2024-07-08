a9a9bfcf4ae7d3f3d94424c561fac9177668adfd
Bruna Vitória <brunavitoriati@gmail.com>
2024-07-08 20:55:31 -0300

    feat: Cria um esqueleto da página que exibirá o formulário

---
+++ symfony/assets/vue/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+      <component :is="'Step' + step" @next="nextStep" :userId="userId"></component>
+    </div>
+</template>
+  
+<script>
+import Step1 from './Step1.vue';
+import Step2 from './Step2.vue';
+import Step3 from './Step3.vue';
+  
+export default {
+    data() {
+        return {
+        step: 1,
+        userId: null,
+        };
+    },
+    components: {
+        Step1,
+        Step2,
+        Step3,
+    },
+    methods: {
+        nextStep(userId) {
+        this.userId = userId;
+        this.step++;
+        },
+    },
+};
+</script>
+  
